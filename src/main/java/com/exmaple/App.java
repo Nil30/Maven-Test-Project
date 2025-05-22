a1671f2a5efeb952268a1363f7da556c747bac19
Nilesh Patil <nilesh.patil21@ad.infosys.com>
2025-05-22 13:21:06 +0530

    First commit

---
+++ src/main/java/com/exmaple/App.java
@@ -0,0 +1,14 @@
+package com.exmaple;
+
+public class App {
+	public static void main(String[] args) {
+
+		System.out.println("Started the code");
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.print(i + " ");
+		}
+		
+		System.out.println("\nEnded the code");
+	}
+}
